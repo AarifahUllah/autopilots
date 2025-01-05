8982e56e4ea527955a7783426f749ea6cd6f713a
AarifahUllah <112424436+AarifahUllah@users.noreply.github.com>
2025-01-05 14:09:54 -0500

    Update mission.py

--- DroneKit & MAVLink & pymavlink/mission.py
+++ DroneKit & MAVLink & pymavlink/mission.py
@@ -1,84 +1,22 @@
+"""
+Testing AUTO scripts for drone simulation using pymavlink and DroneKit
+Based on DroneKit's mission_basic.py
+"""
+
+#############DEPENDENCIES#############
 from dronekit import connect, VehicleMode, LocationGlobalRelative
 from pymavlink import mavutil, mavwp
 import time
+import argparse
 
-"""
-def arm_and_takeoff(vehicle, altitude):
-
-    #Arm the vehicle and take off to the specified altitude.
-
-    print("Arming the vehicle...")
-
-    vehicle.mav.command_long_send(vehicle.target_system, vehicle.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-
-    print("Waiting for the vehicle to arm")
-
-    while not vehicle.motors_armed():
-
-        time.sleep(1)
-
-    print("Taking off...")
-
-    vehicle.mav.command_long_send( vehicle.target_system, vehicle.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, altitude)
- 
-def create_waypoint(lat, lon, alt):
-
-    #Create a MAVLink waypoint message.
-
-    return mavutil.mavlink.MAVLink_mission_item_message(
-
-        0, 0, 0,
-
-        mavutil.mavlink.MAV_FRAME_GLOBAL_RELATIVE_ALT,
-
-        mavutil.mavlink.MAV_CMD_NAV_WAYPOINT,
-
-        0, 0, 0, 0, 0, 0,
-
-        lat, lon, alt
-
-    )
- 
-def upload_mission(vehicle, waypoints):
-
-    #Upload the mission waypoints to the vehicle.
-
-    print("Uploading mission...")
-
-    vehicle.waypoint_clear_all_send()
-
-    vehicle.waypoint_count_send(len(waypoints))
-
-    for i, wp in enumerate(waypoints):
-
-        vehicle.mav.send(wp)
-
-        print(f"Waypoint {i+1} sent")
-"""
-
-# Start a connection listening on a UDP port
-#the_connection = mavutil.mavlink_connection('udpin:localhost:14551')
-
-# Wait for the first heartbeat
-# This sets the system and component ID of remote system for the link
-#the_connection.wait_heartbeat()
-#print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
-
-#the_connection = "--connect udpin:localhost:14551".connect
-import argparse  
+#############CONNECTION#############
 parser = argparse.ArgumentParser(description='Demonstrates basic mission operations.')
 parser.add_argument('--connect', 
                    help="vehicle connection target string. If not specified, SITL automatically started and used.")
 args = parser.parse_args()
 
 connection_string = args.connect
-sitl = None
 
-#Start SITL if no connection string specified
-if not connection_string:
-    import dronekit_sitl
-    sitl = dronekit_sitl.start_default()
-    connection_string = sitl.connection_string()
 
 # Connect to the Vehicle
 print('Connecting to vehicle on: %s' % connection_string)
@@ -209,47 +147,6 @@ def main():
     #Close vehicle object before exiting script
     print("Close vehicle object")
     the_connection.close()
-
-    # Shut down simulator if it was started.
-    if sitl is not None:
-    sitl.stop()
-
-    """
-    #Set the flight mode of the vehicle. Mode ID (1 for AUTO)
-    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_DO_SET_MODE, 0, 1, 0, 0, 0, 0, 0, 0)
-    msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-    print(msg)
-
-    # Once connected, use 'the_connection' to get and send messages
-    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM, 0, 1, 0, 0, 0, 0, 0, 0)
-    msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-    print(msg)
-
-    #Take off 10 meters
-    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_TAKEOFF, 0, 0, 0, 0, 0, 0, 0, 10)
-    msg = the_connection.recv_match(type='COMMAND_ACK', blocking=True)
-    print(msg)
- 
-    print("Mission in progress...")
-
-    while True:
-
-        # Monitor mission progress (e.g., check for mission completion)
-
-        msg = the_connection.recv_match(type="MISSION_ITEM_REACHED", blocking=True)
-
-        print(f"Reached waypoint {msg.seq}")
-
-        if msg.seq == len(waypoints) - 1:
-
-            print("Mission completed")
-
-            break
- 
-    print("Landing...")
-
-    the_connection.mav.command_long_send(the_connection.target_system, the_connection.target_component, mavutil.mavlink.MAV_CMD_NAV_LAND, 0, 0, 0, 0, 0, 0, 0)
-    """
  
 if __name__ == "__main__":
 
